66e0efa87f169e143ff6a7899542e02cbf89c913
rcdmk <rcdmk@dtavares.com>
2015-09-25 22:11:48 -0300

    Base board implementation

---
+++ src/Board.js
@@ -0,0 +1,73 @@
+function Board() {
+	'use strict';
+	
+	var self = this;
+	var map = [];
+	
+	self.INVALID_COORDINATES_ERROR = 'Invalid or out of range coordinates: ';
+	
+
+	self.init = function() {
+		map = [
+			[0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0, 0]
+		];		
+	};
+	
+	self.getMap = function() {
+		return map.slice();
+	};
+	
+	self.setNumber = function(x, y, number) {
+		checkCoordinates(x, y);
+		
+		map[y][x] = number;
+	};
+	
+	self.getNumber = function(x, y) {
+		checkCoordinates(x, y);
+
+		return map[y][x];
+	};
+	
+	self.checkLine = function(y, number) {
+		var linha = map[y];
+		
+		return linha.indexOf(number) === -1;
+	};
+	
+	self.checkColumn = function(x, number) {
+		var coluna = map.map(function(el, i) {
+			return el[x];
+		});
+		
+		return coluna.indexOf(number) === -1;
+	};
+	
+	function checkValidNumber(number) {
+		return /^\d+$/.test(String(number));
+	}
+	
+	function checkX(x) {
+		return checkValidNumber(x) && (x >= 0 && x < map[0].length);
+	}
+	
+	function checkY(y) {
+		return checkValidNumber(y) && (y >= 0 && y < map.length);
+	}
+	
+	function checkCoordinates(x, y) {
+		if (!checkX(x) || !checkY(y)) {
+			throw new Error(INVALID_COORDINATES_ERROR + x + ', ' + y);
+		}
+	}
+}
+
+module.exports = new Board();
